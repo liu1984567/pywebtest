d3d41213a4e888fe94b66ec8d2a61430875b9198
liudh <liudh@inspur.com>
2019-04-22 23:02:53 +0800

    1. Add some dir 2. add login welcom html

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 #app.py
 
 def application(environ, start_response):
+    #print(environ)
+    method = environ['REQUEST_METHOD']
+    contenttype = environ['CONTENT_TYPE']
+    pathinfo = environ['PATH_INFO']
+    print(pathinfo)
+    querystr = environ['QUERY_STRING']
+    print(querystr)
     start_response('200 OK', [('Content-Type', 'text/html')])
+    #deal login
+    if pathinfo == '/login/':
+        return [b'<h1>Welcome, please login first!</h1>']
+    #default deal
     return [b'<h1>Hello, web!</h1>']
